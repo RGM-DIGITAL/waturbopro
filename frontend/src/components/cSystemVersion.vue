834b9d91923502e6d4bf1387d48d1ac940548306
Rafael Matos <30558052+rafaelmatostj@users.noreply.github.com>
2024-07-13 09:23:19 -0300

    tes

--- frontend/src/components/cSystemVersion.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="text-caption text-center q-pa-sm">
-    Versão WhatsFuture:
-    <q-badge align="middle"
-      color="primary">
-     <a target="_blank">1.0</a>
-    </q-badge>
-  </div>
-</template>
