ca99021f3dd3a9c653b9ff6f8fa0fb2b6c95bd21
Rafael Matos <30558052+rafaelmatostj@users.noreply.github.com>
2024-07-16 04:51:53 -0300

    Update cSystemVersion.vue

--- frontend/src/components/cSystemVersion.vue
+++ frontend/src/components/cSystemVersion.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="text-caption text-center bg-grey-1 q-pa-sm">
-    
     <q-badge align="middle"
       color="primary">
       Versão Sistema:v0.01
     </q-badge>
   </div>
 </template>
-<script>
-import packageEnv from 'src/../package.json'
-export default {
-  name: 'SystemVersion',
-  computed: {
-    cVersion () {
-      return packageEnv.version
-    }
-  }
-}
-</script>
+
 <style>
 
 </style>
